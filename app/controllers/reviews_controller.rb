ba383b220b47eae41c89e7e398cf7115b98a7f01
senyokd <senyodzantoh@gmail.com>
2020-12-29 01:44:33 +0000

    added reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -28,8 +28,8 @@ class ReviewsController < ApplicationController
 
     respond_to do |format|
       if @review.save
-        format.html { redirect_to @review, notice: 'Review was successfully created.' }
-        format.json { render :show, status: :created, location: @review }
+        format.html { redirect_to @review.movie, notice: 'Review was successfully created.' }
+        format.json { render :show, status: :created, location: @review.movie }
       else
         format.html { render :new }
         format.json { render json: @review.errors, status: :unprocessable_entity }
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
 
-  has_many :reviews
+  has_many :reviews, dependent: :destroy
   has_many :movies
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
